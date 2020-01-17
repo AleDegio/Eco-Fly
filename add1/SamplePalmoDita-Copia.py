3e63d1f47f3becb3352cb0187dc6a4166b93597c
AleDegio <50446554+AleDegio@users.noreply.github.com>
2020-01-17 08:16:38 +0100

    Add files via upload

--- add1/SamplePalmoDita-Copia.py
+++ add1/SamplePalmoDita-Copia.py
@@ -6,23 +6,17 @@ from threading import Thread
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 from djitellopy.tello import Tello
 
+COMMAND_INTERVAL = 5.0
 
 class SampleListener(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
     tello = Tello(host='192.168.10.1', port=8889)
     tello.connect()
     commandTime = time.clock()
-    takeoff = False
-
-    if takeoff == False:
-        tello.takeoff()
-        takeoff = True
-
 
     def on_frame(self, controller):
         # Get the most recent frame and report some basic information
         frame = controller.frame()
-
 
         position={"Thumb":0,"Index":0,"Middle":0,"Ring":0,"Pinky":0}
 
@@ -30,30 +24,29 @@ class SampleListener(Leap.Listener):
         for hand in frame.hands:
             # Get center hand
             Center= hand.palm_position
-            print Center
-            self.tello.go_xyz_speed(int(Center[0]), int(Center[1]), int(Center[2]), 10)
 
-            if Center[1] >= 355:
-                self.tello.land()
-            
-            time.sleep(2)
-            '''
             # Get fingers
             for finger in hand.fingers:
                 bone = finger.bone(3)
                 position[self.finger_names[finger.type]]=math.sqrt(pow((bone.next_joint[0]-Center[0]),2)+pow((bone.next_joint[1]-Center[1]),2)+pow((bone.next_joint[2]-Center[2]),2))
         for k,v in position.items():
             print k,v
+        if(position["Pinky"] >= 30 and position["Pinky"] <= 85 and (time.clock()-self.commandTime) >= COMMAND_INTERVAL):
+            self.commandTime = time.clock()
+            self.tello.takeoff()
+        elif(position["Thumb"] >= 40 and position["Thumb"] <= 80 and (time.clock()-self.commandTime) >= COMMAND_INTERVAL): 
+            self.commandTime = time.clock
+            self.tello.land()
+        '''elif(position["Index"] >= 40 and position["Index"] <= 110 and position["Middle"] >= 86.0 and position["Middle"] <= 110.0 and (time.clock()-self.commandTime) >= COMMAND_INTERVAL): 
+            self.commandTime = time.clock()
+            self.tello.move_up(50)
+        elif(position["Ring"] >= 30 and position["Ring"] <= 50 and (time.clock()-self.commandTime) >= COMMAND_INTERVAL):
+            self.commandTime = time.clock()
+            self.tello.move_down(50)
+        elif(position["Index"] >= 30 and position["Index"] <= 110  and (time.clock()-self.commandTime) >= COMMAND_INTERVAL):
+            self.commandTime = time.clock()
+            self.tello.move_forward(20)'''
             
-        '''
-            
-               
-
-        
-         
-        
-
-
 def main():
     # Create a sample listener and controller
     listener = SampleListener()
